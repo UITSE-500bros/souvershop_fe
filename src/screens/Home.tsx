dad66f3a1a8b3c0889e65af134034141df5015d9
ThInhHuYnh04 <22521406@gm.uit.edu.vn>
2024-10-17 21:15:32 +0700

    feat: banner text and button

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -15,21 +15,54 @@ const Home = () => {
           alt="home-banner"
           className="absolute left-0 top-0 h-[550px] w-full"
         />
-        <div className="absolute left-[53px] top-[48px] flex w-[635px] h-60 text-black font-[700] text-6xl">
+        <div className="absolute text-center left-[53px] top-[48px] flex h-60 w-[635px] text-6xl font-[700] text-black">
           Khám phá những xu hướng mới nhất
         </div>
         <Button
-        variant="contained"
-        sx={{
-          width:210,
-          height:50,
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
+          variant="contained"
+          sx={{
+            width: 210,
+            height: 50,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: 3,
+            px: 6,
+            py: 1,
+            backgroundColor: "#dbad34",
+            borderRadius: "20px",
+            overflow: "hidden",
+            fontWeight: 500,
+            fontSize: 20,
+            color: "white",
+            textTransform: "none",
+            position: "absolute",
+            marginLeft: "243px",
+            marginTop:35,
+          }}
         >
           Mua ngay
         </Button>
+        <div className="absolute inline-flex h-[68] items-center gap-8 left-[100px] top-[400px] ">
+          <div className="flex-col items-start justify-start">
+            <div className="text-[40px] font-bold text-black">200+</div>
+            <div className="text-base font-normal leading-snug text-black/60">
+              Nhãn hiệu
+            </div>
+          </div>
+          <div className="flex-col items-start justify-start">
+            <div className="text-[40px] font-bold text-black">2,000+</div>
+            <div className="text-base font-normal leading-snug text-black/60">
+              Sản phẩm chất lượng cao
+            </div>
+          </div>
+          <div className="flex-col items-start justify-start">
+            <div className="text-[40px] font-bold text-black">30,000+</div>
+            <div className="text-base font-normal leading-snug text-black/60">
+              Khách hàng hài lòng
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
