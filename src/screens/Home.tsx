a95a94dc52578d895e6faa3d1383eb450f0fd1db
ThInhHuYnh04 <22521406@gm.uit.edu.vn>
2024-10-17 21:27:31 +0700

    Update homepage

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -9,13 +9,15 @@ const Home = () => {
         <Header />
       </div>
 
-      <div className="relative h-[665px] w-full">
+      {/* banner block */}
+
+      <div className="relative h-[550px] w-full">
         <img
           src={MainBanner}
           alt="home-banner"
           className="absolute left-0 top-0 h-[550px] w-full"
         />
-        <div className="absolute text-center left-[53px] top-[48px] flex h-60 w-[635px] text-6xl font-[700] text-black">
+        <div className="absolute left-[53px] top-[48px] flex h-60 w-[635px] text-center text-6xl font-[700] text-black">
           Khám phá những xu hướng mới nhất
         </div>
         <Button
@@ -38,12 +40,13 @@ const Home = () => {
             textTransform: "none",
             position: "absolute",
             marginLeft: "243px",
-            marginTop:35,
+            marginTop: 35,
           }}
         >
           Mua ngay
         </Button>
-        <div className="absolute inline-flex h-[68] items-center gap-8 left-[100px] top-[400px] ">
+
+        <div className="absolute left-[100px] top-[400px] inline-flex h-[68] items-center gap-8">
           <div className="flex-col items-start justify-start">
             <div className="text-[40px] font-bold text-black">200+</div>
             <div className="text-base font-normal leading-snug text-black/60">
@@ -62,6 +65,16 @@ const Home = () => {
               Khách hàng hài lòng
             </div>
           </div>
+        </div>
+      </div>
+
+      {/* empty space */}
+      <div className="h-[100px] w-full bg-[#7b5c43]" />
+
+      {/* new products */}
+      <div className="flex h-[530px] w-full flex-col items-center justify-center">
+        <div className="text-center text-5xl font-bold text-black">
+          Sản phẩm mới
         </div>
       </div>
     </div>
