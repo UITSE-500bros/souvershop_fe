db32d6835a67acbc85f4b11cae67096fc47996d2
Đoàn Phương Nam <120766900+DPhngNam@users.noreply.github.com>
2024-10-27 01:40:45 +0700

    make it better

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./screens/Home";
+import { Route, Routes } from "react-router-dom";
 import Cart from "./screens/CartPage";
 import CategoryPage from "./screens/CategoryPage";
-import CustomerInfoPage from "./screens/CustomerInfoPage";
 import CheckoutPage from "./screens/CheckoutPage";
+import CustomerInfoPage from "./screens/CustomerInfoPage";
+import Home from "./screens/Home";
 
 function App() {
   return (
