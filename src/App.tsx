ca30784ba7a96cdb2aaca9399e5392bffdd7abee
Đoàn Phương Nam <120766900+DPhngNam@users.noreply.github.com>
2024-10-26 23:40:43 +0700

    feat: finished demo

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,19 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./screens/Home";
 import Cart from "./screens/CartPage";
 import CategoryPage from "./screens/CategoryPage";
+import CustomerInfoPage from "./screens/CustomerInfoPage";
+import CheckoutPage from "./screens/CheckoutPage";
 
 function App() {
   return (
     <Router>
-    
-
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="Cart" element={<Cart />} />
         <Route path="Category" element={<CategoryPage />} />
-
+        <Route path="/customer" element={<CustomerInfoPage />} />
+        <Route path="/checkout" element={<CheckoutPage/>} />
         <Route path="*" element={<h1>Not Found</h1>} />
-
-
       </Routes>
     </Router>
   );
