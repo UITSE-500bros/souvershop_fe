c63ab64d4e733c064755896dd73eda1f30644376
ThInhHuYnh04 <22521406@gm.uit.edu.vn>
2024-11-01 20:45:15 +0700

    Feat: init product detail page

---
+++ src/screens/ProductDetail/ProductDetail.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function ProductDetail() {
+  return (
+    <div className="flex flex-col px-[100px]">
+      <div className="routebar inline-flex items-center gap-3">
+        <p>Trang chủ</p>
+        <p>/</p>
+        <p>Shop</p>
+        <p>/</p>
+        <p>Thời trang nam</p>
+        <p>/</p>
+        <p>Áo phông</p>
+      </div>
+
+      {/* Product Detail */}
+    </div>
+  );
+}
+
+export default ProductDetail;
