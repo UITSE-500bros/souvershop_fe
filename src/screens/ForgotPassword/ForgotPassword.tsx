1002c2900db01484ab06dc6b9b7059e402f9cbf6
ThinhHuynhUIT <22521406@gm.uit.edu.vn>
2024-11-17 22:25:43 +0700

    Feat: draft text

--- src/screens/ForgotPassword/ForgotPassword.tsx
+++ src/screens/ForgotPassword/ForgotPassword.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
 
 export default function ForgotPassword() {
   return (
-    <div>
-      h
+    <div className="flex items-center justify-center">
+      <div className="h-[535px] w-[657px] gap-8 bg-[#F8F2E5] px-[60px] py-[80px]">
+        <div className="text-[32px] font-bold text-[#153060]">SouverShop</div>
+
+        <div className="font-['Open Sans'] w-[537px] text-[45px] font-normal leading-[52px] text-[#153060]">
+          Quên mật khẩu
+        </div>
+
+        <div className="font-['Open Sans'] w-[537px] text-base font-normal text-[#828282]">
+          Nhập email bạn đã đăng ký, chúng tôi sẽ gửi đường link để xác nhận
+        </div>
+
+        
+      </div>
     </div>
-  )
+  );
 }
