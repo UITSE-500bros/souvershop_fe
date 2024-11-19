5194baefd81cb85a3187309badcef5f0445a77a5
ThinhHuynhUIT <22521406@gm.uit.edu.vn>
2024-11-19 21:47:56 +0700

    feat: button and textfield

--- src/screens/ForgotPassword/ForgotPassword.tsx
+++ src/screens/ForgotPassword/ForgotPassword.tsx
@@ -1,9 +1,11 @@
+import { SearchBar } from "@/components";
+import { Button, TextField } from "@mui/material";
 import React from "react";
 
 export default function ForgotPassword() {
   return (
-    <div className="flex items-center justify-center">
-      <div className="h-[535px] w-[657px] gap-8 bg-[#F8F2E5] px-[60px] py-[80px]">
+    <div className="flex min-h-screen items-center justify-center">
+      <div className="flex h-[535px] w-[657px] flex-col gap-8 bg-[#F8F2E5] px-[60px] py-[80px]">
         <div className="text-[32px] font-bold text-[#153060]">SouverShop</div>
 
         <div className="font-['Open Sans'] w-[537px] text-[45px] font-normal leading-[52px] text-[#153060]">
@@ -14,7 +16,37 @@ export default function ForgotPassword() {
           Nhập email bạn đã đăng ký, chúng tôi sẽ gửi đường link để xác nhận
         </div>
 
-        
+        <TextField
+          fullWidth
+          label={"Email"}
+          placeholder="Nhập email"
+          variant="outlined"
+          sx={{
+            "& .MuiOutlinedInput-root": {
+              "&.Mui-focused fieldset": {
+                borderColor: "#153060", // Change the border color when focused
+              },
+            },
+            "& .MuiInputLabel-root": {
+              "&.Mui-focused": {
+                color: "#153060", // Change the label color when focused
+              },
+            },
+          }}
+        />
+
+        <Button
+          variant="contained"
+          color="primary"
+          sx={{
+            backgroundColor: "#FFC633",
+            color: "#fff",
+            borderRadius: "10px",
+           
+          }}
+        >
+          Tiếp Tục
+        </Button>
       </div>
     </div>
   );
