6c0ab6d04a8d207aceed157f6b8787e4ba511574
ThinhHuynhUIT <22521406@gm.uit.edu.vn>
2024-11-24 23:02:10 +0700

    style: update icon

--- src/admin/layouts/AdminSidebar/AdminSidebar.tsx
+++ src/admin/layouts/AdminSidebar/AdminSidebar.tsx
@@ -3,47 +3,70 @@ import {
   SidebarContent,
   SidebarGroup,
   SidebarGroupContent,
+  SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
+
+import {
+  House,
+  ChartArea,
+  PackagePlus,
+  CircleUserRound,
+  Database,
+} from "lucide-react";
 import React from "react";
 //menu items
 const items = [
   {
     title: "Trang chính",
     url: "#",
+    icon: House,
   },
   {
     title: "Kho đồ",
     url: "#",
+    icon: Database,
   },
   {
     title: "Thống kê",
     url: "#",
+    icon: ChartArea,
   },
   {
     title: "Nhân viên",
     url: "#",
+    icon: CircleUserRound,
   },
 
   {
     title: "Nhập hàng",
     url: "#",
+    icon: PackagePlus,
   },
 ];
 
 export default function AdminSidebar() {
   return (
     <Sidebar>
+      {/* Sidebar header */}
+      <SidebarHeader>
+        <div className="text-center font-['Inter'] text-3xl font-semibold leading-[30px] text-[#156fee]">
+          SouverShop
+        </div>
+      </SidebarHeader>
+
+      {/* Sidebar content */}
       <SidebarContent>
         <SidebarGroup>
           <SidebarGroupContent>
             <SidebarMenu>
               {items.map((item) => (
                 <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <a href={item.url}>
+                  <SidebarMenuButton className="h-12" asChild>
+                    <a href={item.url} >
+                      <item.icon className="h-8 w-8" />
                       <span>{item.title}</span>
                     </a>
                   </SidebarMenuButton>
