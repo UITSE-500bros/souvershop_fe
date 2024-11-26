3fb2db8df26c1566cb0622802c9f04f69252ad56
ThinhHuynhUIT <22521406@gm.uit.edu.vn>
2024-11-26 21:37:42 +0700

    chore: fix the extra typo

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
-import { Route, Routes } from "react-router-dom";
-
-import CategoryPage from "./screens/Category/CategoryPage";
-import CheckoutPage from "./screens/CheckOut/CheckoutPage";
-import Home from "./screens/Home";
-
-import Cart from "./screens/Cart";
-import Profile from "./screens/CustomerInfo";
-import Headers from "./Layout/Header/Header";
-import Footer from "./Layout/Footer/Footer";
-import ProductDetail from "./screens/ProductDetail";
-import MainRoute from "./routes/MainRoute";
 import ScrollToTop from "./components/ScrollToTop";
+import MainRoute from "./routes/MainRoute";
 
 function App() {
   return (
     <>
       <ScrollToTop />
-      <MainRoute />;
+      <MainRoute />
     </>
   );
 }
