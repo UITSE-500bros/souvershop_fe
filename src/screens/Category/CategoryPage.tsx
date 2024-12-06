a1ac09c14c175b1ef145fc6f3155e2990b81a16f
YA DAT <22520243@gm.uit.edu.vn>
2024-12-06 18:49:21 +0700

    update-style
    update style và chuyển đến trang checkout

--- src/screens/Category/CategoryPage.tsx
+++ src/screens/Category/CategoryPage.tsx
@@ -12,66 +12,39 @@ const CategoryPage: React.FC = () => {
 
   return (
     <div className="flex flex-col min-h-screen bg-[#F8F2E5]">
-      
-      <div className="flex-grow">
-        <div className="my-4 flex items-center text-sm text-gray-600">
-          Trang chủ <FaChevronRight className="mx-2 inline-block" /> Truyền thống
+      <div className="my-4 flex items-center text-sm text-gray-600">
+        Trang chủ <FaChevronRight className="mx-2 inline-block" /> Truyền thống
+      </div>
+
+      <div className="w-full bg-gray-100 p-4 rounded-md mb-6">
+        <div className="flex items-center">
+          <button className="flex items-center space-x-2 text-gray-700 font-semibold text-lg shadow-md rounded-md px-4 py-2 bg-white">
+          <RiFilterLine className="text-2xl" />
+            <span>Bộ lọc</span>
+          </button>
         </div>
+      </div>
 
-        <div className="flex items-start">
-          
-          <div className="w-1/4 rounded-md bg-gray-100 p-4 space-y-4">
-            <div className="flex items-center justify-between">
-              <p className="font-semibold">Bộ lọc</p>
-              <RiFilterLine className="text-xl" />
-            </div>
 
-            <div className="flex justify-center">
-              <label htmlFor="price-range" className="text-sm">
-                Giá: 0 đ - {priceRange.toLocaleString()} đ
-              </label>
-            </div>
-            <div className="flex items-center whitespace-nowrap">
-              <span>0 đ</span>
-              <input
-                id="price-range"
-                type="range"
-                min="0"
-                max="1000000"
-                value={priceRange}
-                onChange={handlePriceRangeChange}
-                className="mx-4 w-full"
-              />
-              <span>1,000,000 đ</span>
-            </div>
-            <div className="mt-4 flex justify-center">
-              <button className="rounded-[62px] bg-[#DBAD34] px-4 py-2 text-white">
-                Áp dụng bộ lọc
-              </button>
-            </div>
-          </div>
+      {/* Phần sản phẩm */}
+      <div className="flex-grow">
+        <h2 className="ml-4 text-2xl font-bold">Truyền thống</h2>
 
-          {/* Phần sản phẩm */}
-          <div className="flex-grow">
-            <h2 className="ml-4 text-2xl font-bold">Truyền thống</h2>
-
-            <div className="grid w-full grid-cols-3 gap-1 p-2">
-              <ProductCard
-                imageUrl="https://cdn.ready-market.com.tw/24cfa4d4/Templates/pic/m/img_product_3462_20140903175702-1256-9615.jpg?v=c58ca81e"
-                name="Móc khóa"
-                price={250000}
-                discountPrice={200000}
-                rating={4}
-              />
-              <ProductCard
-                imageUrl="https://mysofa.vn/wp-content/uploads/2020/02/cac-mau-tranh-thac-nuoc-phong-thuy-dep-nhat-37.jpg"
-                name="Tranh Đông Hồ"
-                price={500000}
-                discountPrice={400000}
-                rating={5}
-              />
-            </div>
-          </div>
+        <div className="grid w-full grid-cols-3 gap-2 px-2 py-4">
+          <ProductCard
+            imageUrl="https://cdn.ready-market.com.tw/24cfa4d4/Templates/pic/m/img_product_3462_20140903175702-1256-9615.jpg?v=c58ca81e"
+            name="Móc khóa"
+            price={250000}
+            discountPrice={200000}
+            rating={4}
+          />
+          <ProductCard
+            imageUrl="https://mysofa.vn/wp-content/uploads/2020/02/cac-mau-tranh-thac-nuoc-phong-thuy-dep-nhat-37.jpg"
+            name="Tranh Đông Hồ"
+            price={500000}
+            discountPrice={400000}
+            rating={5}
+          />
         </div>
       </div>
     </div>
