5f22a3a2003c2d0e5ab030cd0c2f50407fc43ee5
ThinhHuynhUIT <22521406@gm.uit.edu.vn>
2024-12-14 20:44:05 +0700

    feat: implement  get all category

---
+++ src/models/Product.ts
@@ -0,0 +1,16 @@
+export interface Product {
+  product_id: string;
+  product_name: string;
+  product_selling_price: number;
+  product_import_price: number;
+  product_quantity: number;
+  product_image: string[];
+  product_describe: string;
+  is_favourited: boolean;
+  is_sale: boolean;
+  percentage_sale: number;
+  average_rating: number | null;
+  create_at: string;
+  update_at: string;
+  category_id: number;
+}
--- src/screens/Category/CategoryPage.tsx
+++ src/screens/Category/CategoryPage.tsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { FaChevronRight } from "react-icons/fa";
 import { RiFilterLine } from "react-icons/ri";
-import ProductCard, { ProductCardProps } from "../../components/ProductCard";
+import ProductCard from "../../components/ProductCard";
 import { getAllProducts } from "./service/category.service";
 import { Loading } from "@/components/Loading";
+import { Product } from "@/models/Product";
 
 const CategoryPage: React.FC = () => {
   const [priceRange, setPriceRange] = useState(1000000);
-  const [products, setProducts] = useState<ProductCardProps[]>([]);
+  const [products, setProducts] = useState<Product[]>([]);
 
   const handlePriceRangeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPriceRange(Number(e.target.value));
@@ -16,13 +17,12 @@ const CategoryPage: React.FC = () => {
   useEffect(() => {
     const fetchProducts = async () => {
       const products = await getAllProducts();
-      if(!products) return;
+      if (!products) return;
       setProducts(products);
     };
     fetchProducts();
     console.log(products);
-    
-  },[products]);
+  }, [products]);
 
   if (!products) {
     return <Loading />;
@@ -34,7 +34,7 @@ const CategoryPage: React.FC = () => {
         Trang chủ <FaChevronRight className="mx-2 inline-block" /> Truyền thống
       </div>
 
-      <div className="mb-6 w-full rounded-md bg-gray-100 p-4">
+      <div className="mb-6 w-full rounded-md bg-gray-100 p-5">
         <div className="flex justify-end">
           <button className="flex items-center space-x-2 rounded-md bg-white px-4 py-2 text-lg font-semibold text-gray-700 shadow-md">
             <RiFilterLine className="text-2xl" />
@@ -47,8 +47,21 @@ const CategoryPage: React.FC = () => {
       <div className="flex-grow">
         <h2 className="ml-4 text-2xl font-bold">Truyền thống</h2>
 
-        <div className="grid w-full grid-cols-3 gap-1 p-2">
-         
+        <div className="grid w-full grid-cols-4 gap-1 p-2">
+          {products.map((product) => {
+            return (
+              <ProductCard
+                key={product.product_id}
+                name={product.product_name}
+                price={product.product_selling_price}
+                imageUrl={product.product_image[0]}
+                rating={
+                  product.average_rating === null ? 0 : product.average_rating
+                }
+                isFavorite={false}
+              />
+            );
+          })}
         </div>
       </div>
     </div>
