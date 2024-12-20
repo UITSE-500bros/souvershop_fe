13f21de5aee03537a01844958ab5eae52416e703
YA DAT <22520243@gm.uit.edu.vn>
2024-12-21 03:04:39 +0700

    fix-style
    fix style inventory

--- src/admin/screens/Inventory/Inventory.tsx
+++ src/admin/screens/Inventory/Inventory.tsx
@@ -7,7 +7,7 @@ export default function Inventory() {
   const [isAddingInventory, setIsAddingInventory] = useState(false);
 
   const handleAddInventory = () => {
-    setIsAddingInventory(true);  
+    setIsAddingInventory(true);
   };
 
   const handleCloseModal = () => {
@@ -17,6 +17,54 @@ export default function Inventory() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-[#f5f5f5]">
       <div className="flex h-[700px] w-[1096px] flex-col gap-8 bg-white px-[20px] py-[20px] shadow-md relative">
+        <div className="flex flex-col items-start p-4 border border-[#ddd] rounded-md">
+          <div className="text-[24px] font-bold text-[#333] text-left">Tổng kho</div>
+
+          <div className="flex gap-[75px] mt-2 justify-start">
+            <div className="flex flex-col items-start justify-start w-[200px] p-[10px]  rounded-md gap-y-2">
+              <div className="text-[#1570EF] font-bold">Thể loại</div>
+              <div className="text-[14px] font-bold">7</div>
+              <div className="text-[#858D9D] text-[14px]">7 ngày qua</div>
+            </div>
+
+            <div className="flex flex-col items-start justify-start w-[200px] p-[10px]  rounded-md">
+              <div className="text-[#E19133] font-bold">Tổng sản phẩm</div>
+              <div className="flex justify-between w-full mt-2">
+                <div className="text-[14px] font-bold">868</div>
+                <div className="text-[14px] font-bold">25,000 VNĐ</div>
+              </div>
+              <div className="flex justify-between w-full mt-2">
+                <div className="text-[#858D9D] text-[14px]">7 ngày qua</div>
+                <div className="text-[#858D9D] text-[14px]">Doanh thu</div>
+              </div>
+            </div>
+
+            <div className="flex flex-col items-start justify-start w-[200px] p-[10px]  rounded-md">
+              <div className="text-[#845EBC] font-bold">Bán chạy nhất</div>
+              <div className="flex justify-between w-full mt-2">
+                <div className="text-[14px] font-bold">2</div>
+                <div className="text-[14px] font-bold">25,000 VNĐ</div>
+              </div>
+              <div className="flex justify-between w-full mt-2">
+                <div className="text-[#858D9D] text-[14px]">7 ngày qua</div>
+                <div className="text-[#858D9D] text-[14px]">Trị giá</div>
+              </div>
+            </div>
+
+            <div className="flex flex-col items-start justify-start w-[200px] p-[10px]  rounded-md">
+              <div className="text-[#F36960] font-bold">Cổ phiếu thấp</div>
+              <div className="flex justify-between w-full mt-2">
+                <div className="text-[14px] font-bold">12</div>
+                <div className="text-[14px] font-bold">2</div>
+              </div>
+              <div className="flex justify-between w-full mt-2">
+                <div className="text-[#858D9D] text-[14px]">Đã đặt hàng</div>
+                <div className="text-[#858D9D] text-[14px]">Không có trong kho</div>
+              </div>
+            </div>
+          </div>
+        </div>
+
         <div className="flex justify-between items-center">
           <div className="text-[24px] font-bold text-[#333]">Các sản phẩm</div>
           <InventoryButton onAddInventory={handleAddInventory} />
@@ -35,7 +83,7 @@ export default function Inventory() {
             <div className="flex p-[10px] border-b border-[#ddd]" key={index}>
               <div className="flex-1 text-left">Maggi</div>
               <div className="flex-1 text-left">250 000 VNĐ</div>
-              <div className="flex-1 text-left">50 GGói</div>
+              <div className="flex-1 text-left">50 Gói</div>
               <div className="flex-1 text-left">15 Gói</div>
               <div className="flex-1 text-left">12/1/2024</div>
               <div className="flex-1 text-left">Còn hàng</div>
@@ -44,9 +92,13 @@ export default function Inventory() {
         </div>
 
         <div className="flex justify-center items-center gap-[20px] p-[10px] mt-[auto]">
-          <Button variant="outlined" sx={{ textTransform: "none", marginRight: "350px" }}>Trước</Button>
+          <Button variant="outlined" sx={{ textTransform: "none", marginRight: "350px" }}>
+            Trước
+          </Button>
           <div>Trang 1 trong 10</div>
-          <Button variant="outlined" sx={{ textTransform: "none", marginLeft: "350px" }}>Sau</Button>
+          <Button variant="outlined" sx={{ textTransform: "none", marginLeft: "350px" }}>
+            Sau
+          </Button>
         </div>
 
         {isAddingInventory && (
@@ -65,15 +117,15 @@ export default function Inventory() {
                   </div>
                 </div>
 
-                <div className="flex justify-between items-center">
+                <div className="flex justify-between items-center gap-3">
                   <label className="text-[14px] text-[#333] w-[150px]">Các sản phẩm:</label>
                   <TextField fullWidth variant="outlined" />
                 </div>
-                <div className="flex justify-between items-center">
+                <div className="flex justify-between items-center gap-3">
                   <label className="text-[14px] text-[#333] w-[150px]">Giá mua:</label>
                   <TextField fullWidth variant="outlined" />
                 </div>
-                <div className="flex justify-between items-center">
+                <div className="flex justify-between items-center gap-3">
                   <label className="text-[14px] text-[#333] w-[150px]">Số lượng:</label>
                   <TextField fullWidth variant="outlined" />
                 </div>
@@ -81,17 +133,21 @@ export default function Inventory() {
                   <label className="text-[14px] text-[#333] w-[100px]">Giá trị ngưỡng:</label>
                   <TextField fullWidth variant="outlined" />
                 </div>
-                <div className="flex justify-between items-center">
+                <div className="flex justify-between items-center gap-3">
                   <label className="text-[14px] text-[#333]">Ngày hết hạn:</label>
                   <TextField sx={{ width: "250px" }} variant="outlined" />
                 </div>
-                <div className="flex justify-between items-center">
-                    <label className="text-[14px] text-[#333]">Khả dụng:</label>
-                    <TextField sx={{ width: "250px" }} variant="outlined" />
+                <div className="flex justify-between items-center gap-3">
+                  <label className="text-[14px] text-[#333]">Khả dụng:</label>
+                  <TextField sx={{ width: "250px" }} variant="outlined" />
                 </div>
-                <div className="flex justify-between mt-4">
-                  <Button variant="outlined" onClick={handleCloseModal}>Hủy bỏ</Button>
-                  <Button variant="contained" color="primary" onClick={handleCloseModal}>Thêm sản phẩm</Button>
+                <div className="flex justify-between mt-4 gap-3">
+                  <Button variant="outlined" onClick={handleCloseModal}>
+                    Hủy bỏ
+                  </Button>
+                  <Button variant="contained" color="primary" onClick={handleCloseModal}>
+                    Thêm sản phẩm
+                  </Button>
                 </div>
               </div>
             </div>
