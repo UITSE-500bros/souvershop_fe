1e353c72736eeaa111e583bf6a995ecf12c42930
ThinhHuynhUIT <22521406@gm.uit.edu.vn>
2024-12-26 20:59:31 +0700

    feat: basic setup for admin dashboard

--- src/admin/AdminApp.tsx
+++ src/admin/AdminApp.tsx
@@ -2,12 +2,13 @@ import Home from "@/screens/Home";
 import { Route, Routes } from "react-router-dom";
 import AdminLayout from "./layouts/AdminLayout";
 import EmployeesList from "./screens/EmployeesList";
+import AdminDashboard from "./screens/DashBoard";
 
 export default function AdminApp() {
   return (
     <Routes>
       <Route path="/" element={<AdminLayout />}>
-        <Route index element={<Home />} />
+        <Route index element={<AdminDashboard />} />
         <Route path="employees" element={<EmployeesList />} />
         {/* Add more nested routes here */}
       </Route>
