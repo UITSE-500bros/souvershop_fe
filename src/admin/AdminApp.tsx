88edb3e90896ae55df702e1f0eb744b882986b1d
YA DAT <22520243@gm.uit.edu.vn>
2024-12-26 22:03:15 +0700

    fix route
    fix inventory route

--- src/admin/AdminApp.tsx
+++ src/admin/AdminApp.tsx
@@ -2,6 +2,7 @@ import Home from "@/screens/Home";
 import { Route, Routes } from "react-router-dom";
 import AdminLayout from "./layouts/AdminLayout";
 import EmployeesList from "./screens/EmployeesList";
+import Inventory from "./screens/Inventory";
 
 export default function AdminApp() {
   return (
@@ -10,6 +11,8 @@ export default function AdminApp() {
         <Route index element={<Home />} />
         <Route path="employees" element={<EmployeesList />} />
         {/* Add more nested routes here */}
+        <Route path="inventory" element={<Inventory/>}/>
+
       </Route>
     </Routes>
   );
--- src/routes/MainRoute.tsx
+++ src/routes/MainRoute.tsx
@@ -11,7 +11,6 @@ import Home from "@/screens/Home";
 import NewPassword from "@/screens/NewPassword";
 import ProductDetail from "@/screens/ProductDetail";
 import EmployeesList from "@/admin/screens/EmployeesList";
-import Inventory from "@/admin/screens/Inventory";
 import { Route, Routes } from "react-router-dom";
 import Login from "@/screens/Login";
 import SignUp from "@/screens/SignUp/SignUp";
@@ -40,7 +39,6 @@ export default function MainRoute() {
 
 
         <Route path="/admin/*" element={<AdminApp />} />
-        <Route path="/inventory" element={<Inventory/>}/>
         <Route path="/employees" element={<EmployeesList />} />
         <Route path="*" element={<h1>Not Found</h1>} />
       </Routes>
