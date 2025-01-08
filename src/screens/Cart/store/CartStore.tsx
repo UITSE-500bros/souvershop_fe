b08b02fd100d6fd11bf874ea77452ec130081473
ThinhHuynhUIT <22521406@gm.uit.edu.vn>
2025-01-09 02:57:53 +0700

    feat: clear cart

--- src/screens/Cart/store/CartStore.tsx
+++ src/screens/Cart/store/CartStore.tsx
@@ -1,12 +1,15 @@
 import { create } from "zustand";
 import CartItem from "@/models/CartItem";
 import { Product } from "@/models/Product";
-import { addProductToCart, getCart, removeProductFromCart } from "../service/Cart.service";
-
+import {
+  addProductToCart,
+  getCart,
+  removeProductFromCart,
+} from "../service/Cart.service";
 
 type CartStore = {
   cartItems: CartItem[];
-  setCartItems: () => void
+  setCartItems: () => void;
   addToCart: (product: Product) => void;
   removeFromCart: (productId: string) => void;
   updateQuantity: (productId: string, quantity: number) => void;
@@ -17,39 +20,51 @@ type CartStore = {
 const useCartStore = create<CartStore>((set, get) => ({
   cartItems: [],
   setCartItems: () => {
-    getCart().then(
-      (res) => {
+    getCart()
+      .then((res) => {
         set(() => ({
-          cartItems: res.products_list.reduce((merged: CartItem[], product: Product) => {
-            const existing = merged.find((item) => item.product_id === product.product_id);
-            if (existing) {
-              existing.quantity += product.quantity || 1;
-              existing.total = existing.product_selling_price * existing.quantity;
-            } else {
-              merged.push({
-                ...product,
-                quantity: product.quantity || 1,
-                total: product.product_selling_price * (product.quantity || 1),
-              });
-            }
-            return merged;
-          }, []),
+          cartItems: res.products_list.reduce(
+            (merged: CartItem[], product: Product) => {
+              const existing = merged.find(
+                (item) => item.product_id === product.product_id,
+              );
+              if (existing) {
+                existing.quantity += product.quantity || 1;
+                existing.total =
+                  existing.product_selling_price * existing.quantity;
+              } else {
+                merged.push({
+                  ...product,
+                  quantity: product.quantity || 1,
+                  total:
+                    product.product_selling_price * (product.quantity || 1),
+                });
+              }
+              return merged;
+            },
+            [],
+          ),
         }));
-      }).catch((err) => console.error(err));
+      })
+      .catch((err) => console.error(err));
   },
   addToCart: (product) => {
-    addProductToCart(product.product_id, 1).then(
-      () => {
+    addProductToCart(product.product_id, 1)
+      .then(() => {
         set((state) => {
           const existingItem = state.cartItems.find(
-            (item) => item.product_id === product.product_id
+            (item) => item.product_id === product.product_id,
           );
           if (existingItem) {
             return {
               cartItems: state.cartItems.map((item) =>
                 item.product_id === product.product_id
-                  ? { ...item, quantity: item.quantity + 1, total: item.product_selling_price * (item.quantity + 1) }
-                  : item
+                  ? {
+                      ...item,
+                      quantity: item.quantity + 1,
+                      total: item.product_selling_price * (item.quantity + 1),
+                    }
+                  : item,
               ),
             };
           }
@@ -60,30 +75,41 @@ const useCartStore = create<CartStore>((set, get) => ({
             ],
           };
         });
-      }
-    ).catch((err) => console.error(err));
+      })
+      .catch((err) => console.error(err));
   },
   removeFromCart: (productId) => {
-    removeProductFromCart(productId).then(
-      () => {
-        set((state) => ({
-          cartItems: state.cartItems.filter((item) => item.product_id !== productId),
-        }));
-      }
-    )
-
+    removeProductFromCart(productId).
+    then((res)=>console.log(res)).
+    then(() => {
+      set((state) => ({
+        cartItems: state.cartItems.filter(
+          (item) => item.product_id !== productId,
+        ),
+      }));
+    });
   },
   updateQuantity: (productId, quantity) => {
     set((state) => ({
       cartItems: state.cartItems.map((item) =>
         item.product_id === productId
           ? { ...item, quantity, total: item.product_selling_price * quantity }
-          : item
+          : item,
       ),
     }));
   },
   clearCart: () => {
-    set({ cartItems: [] });
+    get().cartItems.forEach((item) => {
+      removeProductFromCart(item.product_id).then(() => {
+        set((state) => ({
+          cartItems: state.cartItems.filter(
+            (cartItem) => cartItem.product_id !== item.product_id,
+          ),
+        }));
+      
+      })
+    });
+   
   },
   getLength: () => {
     return get().cartItems.length;
--- src/screens/PaymentReturn/PaymentReturn.tsx
+++ src/screens/PaymentReturn/PaymentReturn.tsx
@@ -5,11 +5,13 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { URL } from "url";
 import useCartStore from "../Cart/store/CartStore";
+import { removeProductFromCart } from "../Cart/service/Cart.service";
 
 const PaymentReturn = () => {
   const location = useLocation();
   const search = location.search;
   const nav = useNavigate()
+  const cartItems = useCartStore((state) => state.cartItems);
   const clearCart = useCartStore((state) => state.clearCart);
   useEffect(() => {
     const fetchPaymentReturn = async () => {
@@ -18,8 +20,15 @@ const PaymentReturn = () => {
         console.log(res);
         if (res.status === 200) {
           toast.success('Đặt hàng thành công');
+          // cartItems.forEach((item) => {
+          //   removeProductFromCart(item.product_id).then(() => {
+          //     console.log('Remove product from cart success');
+          //   }).catch((err) => {
+          //     console.error(err);
+          //   });
+          // });
           clearCart();
-          nav('/'); 
+          nav('/cart'); 
         }
       } catch (error) {
         
